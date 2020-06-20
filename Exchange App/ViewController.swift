8d52f1a12bcdf0e4293bb4e4c29421fff32cbad0
Marius Ilie <marius.ilie@softbinator.com>
2020-06-20 21:50:09 +0300

    Initial Commit

---
+++ Exchange App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Exchange App
+//
+//  Created by Marius Ilie on 20/06/2020.
+//  Copyright © 2020 Marius Ilie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
